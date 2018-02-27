c85c735fcd9fcb2da464966b8a274070a12e7b59
Decaded <Decaded@users.noreply.github.com>
2018-02-27 18:33:12 +0100

    Imma so dumb. Lemme fix.

--- src/Commands/Ping.js
+++ src/Commands/Ping.js
@@ -8,28 +8,8 @@ module.exports = class Help extends Command {
             // this.aliases = [ ] // alias
             // this.premissions = 0b1 // define them later
         this.channels = ["text"] //works only on guild
-
-        this.types = {
-            pong: {
-                "name": {
-                    required: true
-                }
-            },
-            run: {
-                "command": {
-                    required: false,
-                    err: ":|"
-                }
-            }
-        }
-        this.data = {
-            "guildData": 0
-        }
     }
 
-    subhi(msg) {
-        msg.reply("Ping Pong!")
-    }
     async run(msg) {
         msg.reply(`Pong Ping!`)
     }
